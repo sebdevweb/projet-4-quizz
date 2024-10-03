0d0de3623975ba7bd7183d0de7a9a4332f0f4755
sebdevweb <spansini@squad.fr>
2024-10-03 10:15:53 +0200

    [PROJET-4-QUIZZ] Navigate into quizz

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ quiz.title }}</h1>
     <Progress :value="step" :max="quiz.questions.length - 1" />
-    <Question :question="question" v-if="question" @answer="addAnswer" />
+    <Question :question="question" v-if="state === 'question'" @answer="addAnswer" />
     {{ answers }}
   </div>
 </template>
@@ -17,9 +17,18 @@
     quiz: Object
   })
 
+  const state = ref('question')
   const answers = ref(props.quiz.questions.map(() => null))
   const step = ref(0)
   const question = computed(() => props.quiz.questions[step.value])
+  const addAnswer = (answer) => {
+    answers.value[step.value] = answer
+    if (step.value === props.quiz.questions.length - 1) {
+      state.value = 'recap'
+    } else {
+      step.value++
+    }
+  }
 
 </script>
 
